4f85b27272c1edbba7c0b045eddac44db15b7e72
Max-Baka <metal-smaks@hotmail.fr>
2023-01-26 10:00:02 +0000

    php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/*$nb = 42;
+$str = '42';
+$nbstr = ($nb);
+
+echo $nb;
+echo $str;
+echo $nbstr;*/
+
+/*$nb_tab = [33, 34, 4, 2.2, 5.6];
+print_r($nb_tab); 
+$moto = ["Yam", "Honda", "Kawa","Ducati"];
+print_r($moto);*/
+
+/*$animal = [
+    "species" => "Chien",
+    "name" => "Sonic",
+    "age" => "13",
+    ];
+    print_r($animal);*/
+    
+   /* $data = 42;
+    $float = (float)$data;
+    print_r($float);
+    
+    $str = (string)$float;
+    print_r($str);*/
+    
+    $animals = ["Chat", "Chien", "Lapin", "Souris"];
+    foreach($animals as $animal)
+{
+    echo "$animal\n";
+    echo "<br>";
+}
+?>
